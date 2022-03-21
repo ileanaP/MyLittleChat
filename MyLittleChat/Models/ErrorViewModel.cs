61f113a0c375c0d3a40f750607e5344311989ae7
IleanaP <ileana.profeanu@gmail.com>
2022-03-21 14:47:53 +0200

    Init commit

---
+++ MyLittleChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyLittleChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
